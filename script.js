2437db1593d7a55f74ee5ad2b3082977e2dabef0
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2023-07-05 22:33:12 +0530

    ques

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+// Complete the JS code
+
+function Employee(name, salary) {}
+
+Employee.prototype.giveRaise = function (amount) {};
+
+function Manager(name, salary, department) {}
+
+Manager.prototype.giveRaise = function (amount) {};
+
+// do not change the code below this
+window.Employee = Employee;
+window.Manager = Manager;
